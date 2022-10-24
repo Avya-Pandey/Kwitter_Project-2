6d1652ad5ded640b1f731a625ec3e7f3c2deb51a
Avya-Pandey <96517693+Avya-Pandey@users.noreply.github.com>
2022-10-24 15:43:32 +0800

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAMZe902tM6uSQuQGxl6iF2SvoJfof85R8",
+    authDomain: "kwitternew-e4f96.firebaseapp.com",
+    databaseURL: "https://kwitternew-e4f96-default-rtdb.firebaseio.com",
+    projectId: "kwitternew-e4f96",
+    storageBucket: "kwitternew-e4f96.appspot.com",
+    messagingSenderId: "709579725729",
+    appId: "1:709579725729:web:640d013fe8f44c9cf409e0"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+//ADD YOUR FIREBASE LINKS HERE
